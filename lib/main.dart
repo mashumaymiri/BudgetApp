b83b095adad24dc0e6e332c82883154e30816480
abdulaziz-ibrahim27 <abdulaziz.i.j27@gmail.com>
2022-10-29 14:51:16 +0300

    changing from hello to test

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(const MyApp());
 }
 
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: Text('testing'),
         ),
       ),
     );
